6abf816a9c03f2d9e822a7e995734bd24862d28c
Adrian Kunz <clashsoft@hotmail.com>
2020-07-29 11:43:15 +0200

    Add password box route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {PasswordBoxComponent} from "./password-box/password-box.component";
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'password-box', component: PasswordBoxComponent},
 ];
 
 @NgModule({
