1bc2e3dc8f81274ba8e7449ba139609ffa683fc9
Adrian Kunz <clashsoft@hotmail.com>
2020-07-29 12:00:37 +0200

    Implement mailto generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PasswordBoxComponent } from './password-box/password-box.component';
 import { MailtoComponent } from './mailto/mailto.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MailtoComponent } from './mailto/mailto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mailto/mailto.component.ts
+++ src/app/mailto/mailto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mailto',
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mailto.component.scss']
 })
 export class MailtoComponent implements OnInit {
+  subject: string = '';
+  body: string = '';
+  addressList: string = '';
+  variableList: string = '';
+  rows: number = 2;
 
-  constructor() { }
+  links: string[] = [];
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.subject = localStorage.getItem('subjectTemplate') || '';
+    this.body = localStorage.getItem('bodyTemplate') || '';
+    this.addressList = localStorage.getItem('addresses') || '';
+    this.variableList = localStorage.getItem('variables') || '';
+    this.update();
+  }
+
+  update(): void {
+    localStorage.setItem('subjectTemplate', this.subject);
+    localStorage.setItem('bodyTemplate', this.body);
+    localStorage.setItem('addresses', this.addressList);
+    localStorage.setItem('variables', this.variableList);
+
+    const addressLines = this.addressList.split('\n');
+    const variableLines = this.variableList.split('\n');
+
+    const maxRows = Math.max(addressLines.length, variableLines.length);
+    this.rows = maxRows + 2;
+
+    const links: string[] = [];
+    for (let i = 0; i < addressLines.length; i++) {
+      const recipient = addressLines[i];
+      const variables = (variableLines[i] || '').split('\t');
+
+      const subject = this.replaceVariables(this.subject, variables);
+      const body = this.replaceVariables(this.body, variables);
+
+      links.push(`mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`);
+    }
+    this.links = links;
+  }
+
+  replaceVariables(body: string, variables: string[]): string {
+    for (let i = 0; i < variables.length; i++) {
+      body = body.replace(`#${i + 1}#`, variables[i] || '');
+    }
+    return body;
+  }
 }
