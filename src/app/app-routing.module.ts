3e6fa1603e0666b5bead47549ff83244643346f2
Adrian Kunz <clashsoft@hotmail.com>
2020-07-29 12:36:17 +0200

    Add payday2-secret route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {PasswordBoxComponent} from "./password-box/password-box.component";
 import {MailtoComponent} from "./mailto/mailto.component";
+import {Payday2SecretComponent} from "./payday2-secret/payday2-secret.component";
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'password-box', component: PasswordBoxComponent},
   {path: 'mailto', component: MailtoComponent},
+  {path: 'payday2-secret', component: Payday2SecretComponent},
 ];
 
 @NgModule({
