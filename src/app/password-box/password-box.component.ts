1b42e4a95027fead516bd488c915595148008c67
Adrian Kunz <clashsoft@hotmail.com>
2020-07-29 11:38:43 +0200

    Generate password box component

---
+++ src/app/password-box/password-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-box',
+  templateUrl: './password-box.component.html',
+  styleUrls: ['./password-box.component.scss']
+})
+export class PasswordBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
