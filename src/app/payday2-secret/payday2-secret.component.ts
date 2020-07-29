12d74d43c646f010d21ffb2173fff424fcc8d21d
Adrian Kunz <clashsoft@hotmail.com>
2020-07-29 12:55:27 +0200

    Implement modals in payday2-secret

--- src/app/payday2-secret/payday2-secret.component.ts
+++ src/app/payday2-secret/payday2-secret.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-payday2-secret',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Payday2SecretComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  openModal(content): void {
+    this.modalService.open(content, {size: 'lg'});
+  }
 }
