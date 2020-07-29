30e317de970f6207ad7e7edd47074f3e428b9e8b
Adrian Kunz <clashsoft@hotmail.com>
2020-07-29 11:47:07 +0200

    Add mailto route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {PasswordBoxComponent} from "./password-box/password-box.component";
+import {MailtoComponent} from "./mailto/mailto.component";
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'password-box', component: PasswordBoxComponent},
+  {path: 'mailto', component: MailtoComponent},
 ];
 
 @NgModule({
