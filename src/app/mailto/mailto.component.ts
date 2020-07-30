fd6454514e8a0e2b79d578860a9db700c7c7a970
Adrian Kunz <clashsoft@hotmail.com>
2020-07-30 14:56:10 +0200

    Use table for mailto inputs

--- src/app/mailto/mailto.component.ts
+++ src/app/mailto/mailto.component.ts
@@ -8,10 +8,10 @@ import {Component, OnInit} from '@angular/core';
 export class MailtoComponent implements OnInit {
   subject: string = '';
   body: string = '';
-  addressList: string = '';
-  variableList: string = '';
-  rows: number = 2;
 
+  addresses: string[] = [];
+  variableNames: string[] = ['1'];
+  variables: string[][] = [];
   links: string[] = [];
 
   constructor() {
@@ -20,40 +20,53 @@ export class MailtoComponent implements OnInit {
   ngOnInit(): void {
     this.subject = localStorage.getItem('subjectTemplate') || '';
     this.body = localStorage.getItem('bodyTemplate') || '';
-    this.addressList = localStorage.getItem('addresses') || '';
-    this.variableList = localStorage.getItem('variables') || '';
+    this.variableNames = (localStorage.getItem('variableNames') || '').split('\n');
+    this.addresses = (localStorage.getItem('addresses') || '').split('\n');
+    this.variables = (localStorage.getItem('variables') || '').split('\n').map(s => s.split('\t'));
     this.update();
   }
 
   update(): void {
+    console.log('update');
+
+    localStorage.setItem('variableNames', this.variableNames.join('\n'));
     localStorage.setItem('subjectTemplate', this.subject);
     localStorage.setItem('bodyTemplate', this.body);
-    localStorage.setItem('addresses', this.addressList);
-    localStorage.setItem('variables', this.variableList);
-
-    const addressLines = this.addressList.split('\n');
-    const variableLines = this.variableList.split('\n');
-
-    const maxRows = Math.max(addressLines.length, variableLines.length);
-    this.rows = maxRows + 2;
+    localStorage.setItem('addresses', this.addresses.join('\n'));
+    localStorage.setItem('variables', this.variables.map(r => r.join('\t')).join('\n'));
 
     const links: string[] = [];
-    for (let i = 0; i < addressLines.length; i++) {
-      const recipient = addressLines[i];
-      const variables = (variableLines[i] || '').split('\t');
+    for (let i = 0; i < this.addresses.length; i++) {
+      const recipient = this.addresses[i];
+      const variables = this.variables[i];
 
-      const subject = this.replaceVariables(this.subject, variables);
-      const body = this.replaceVariables(this.body, variables);
+      const subject = this.replaceVariables(this.subject, this.variableNames, variables);
+      const body = this.replaceVariables(this.body, this.variableNames, variables);
 
       links.push(`mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`);
     }
     this.links = links;
   }
 
-  replaceVariables(body: string, variables: string[]): string {
+  replaceVariables(body: string, variableNames: string[], variables: string[]): string {
     for (let i = 0; i < variables.length; i++) {
-      body = body.replace(`#${i + 1}#`, variables[i] || '');
+      body = body.replace(`#${variableNames[i]}#`, variables[i] || '');
     }
     return body;
+  }
+
+  addRow(): void {
+    this.variables.push(this.variableNames.slice());
+  }
+
+  addColumn(): void {
+    this.variableNames.push(`${this.variableNames.length}`);
+    for (let row of this.variables) {
+      row.push('');
+    }
+  }
+
+  index(index: number, value: any): number {
+    return index;
   }
 }
