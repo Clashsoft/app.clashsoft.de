5a3d64cc45b993e20e530e21224391f30fa01c14
Adrian Kunz <clashsoft@hotmail.com>
2021-11-13 23:20:35 +0100

    feat(story-tracker): Add timeline component

--- src/app/story-tracker/story-tracker-routing.module.ts
+++ src/app/story-tracker/story-tracker-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TimelineComponent} from './timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'timeline', component: TimelineComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'timeline'},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class StoryTrackerRoutingModule { }
+export class StoryTrackerRoutingModule {
+}
--- src/app/story-tracker/story-tracker.module.ts
+++ src/app/story-tracker/story-tracker.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StoryTrackerRoutingModule } from './story-tracker-routing.module';
+import { TimelineComponent } from './timeline/timeline.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TimelineComponent
+  ],
   imports: [
     CommonModule,
     StoryTrackerRoutingModule
