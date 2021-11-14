717526395afb1008037c2ff001a9ae9ab82f0f1e
Adrian Kunz <clashsoft@hotmail.com>
2021-11-14 12:49:15 +0100

    refactor(story-tracker): Reference constants

--- src/app/story-tracker/timeline/timeline.component.ts
+++ src/app/story-tracker/timeline/timeline.component.ts
@@ -1,29 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Event, Reference} from '../model/event';
+
+const testus: Reference = {type: 'character', id: '0', name: 'Testus'};
+const sword: Reference = {type: 'item', id: '1', name: 'Sword of Swiftness'};
+const testina: Reference = {type: 'character', id: '2', name: 'Testina'};
+const library: Reference = {type: 'location', id: '3', name: 'The Library'};
+
+const references = [
+  testus,
+  sword,
+  testina,
+  library,
+];
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent implements OnInit {
   timeline: Event[] = [
     {
       timestamp: new Date(),
       description: [
-        {type: 'character', id: '0', name: 'Testus'},
+        testus,
         'gives the',
-        {type: 'item', id: '1', name: 'Sword of Swiftness'},
+        sword,
         'to',
-        {type: 'character', id: '2', name: 'Testina'},
+        testina,
         'at',
-        {type: 'location', id: '3', name: 'The Library'},
+        library,
         '.',
       ],
     },
   ];
-
-  constructor() { }
 
   ngOnInit(): void {
   }
