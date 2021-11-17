c155236791f948aec61681f487d64d70d13ce41f
Adrian Kunz <clashsoft@hotmail.com>
2021-11-17 23:33:08 +0100

    feat: Add story API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://led.clashsoft.de/api',
+  storyApiUrl: 'https://clashsoft-stories.herokuapp.com/api/v1',
   vapidPublicKey: 'BL_LSpnzEoqh78x5OhubaG9pFdZcB_V62Xc9y0vnW85gXS28oo5ehUpuFrzYcuBU3WdC-x3kuF49u5hyzwVYbMo',
 };
