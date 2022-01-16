6c25b1d78b48d2290a63264382f12bbe0d72b07e
Adrian Kunz <clashsoft@hotmail.com>
2022-01-16 12:54:03 +0100

    feat(mailto): Use Bootstrap Icons in Mailto

--- src/app/mailto/mailto.module.ts
+++ src/app/mailto/mailto.module.ts
@@ -4,16 +4,18 @@ import {FormsModule} from '@angular/forms';
 
 import {MailtoRoutingModule} from './mailto-routing.module';
 import {MailtoComponent} from './mailto/mailto.component';
+import {NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     MailtoComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    MailtoRoutingModule,
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        MailtoRoutingModule,
+        NgbTooltipModule,
+    ],
 })
 export class MailtoModule {
 }
